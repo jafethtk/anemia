e039af17d56e8d65fbd7bf16e8bc568078e0b68c
Janet Sarmiento <janet.sarmiento@tektonlabs.com>
2018-03-10 01:59:48 -0500

    Add react router

---
+++ src/components/pages/Landing/index.jsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import './styles.scssm';
+
+class Landing extends PureComponent {
+  render() {
+    return (
+      <div>
+        Landing
+      </div>
+    );
+  }
+}
+
+export default Landing;
